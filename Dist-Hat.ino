ab35a09d44e3acff8f971a7aa8a203ad290ecc9c
Collin <79947287+CollinCodez@users.noreply.github.com>
2024-04-20 16:17:43 -0400

    Starting Code

---
+++ Dist-Hat.ino
@@ -0,0 +1,453 @@
+
+
+#include <Arduino.h>
+#include <ArduinoJson.h>			// Library for JSON parsing. This is a more efficient alternative to the arduino built in JSON library
+
+#define ELEGANTOTA_USE_ASYNC_WEBSERVER 1	// Tell ElegantOTA to use the AsyncWebServer
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>		// This will automatically use whatever core is available, at priority 3
+#include <ElegantOTA.h>				// Library for Over The Air (OTA) updates
+
+#include <SPIFFS.h>
+
+
+
+#define SERIAL_ENABLED 1 // Set to 1 to enable Serial Monitor Debugging]
+
+#define NUM_SENSORS 8
+
+
+//======================================================================================================
+//	Hardware Connections
+//======================================================================================================
+
+const gpio_num_t echoPins[] = {GPIO_NUM_36, GPIO_NUM_39, GPIO_NUM_34, GPIO_NUM_35, GPIO_NUM_32, GPIO_NUM_33, GPIO_NUM_25, GPIO_NUM_26};
+
+
+const gpio_num_t MuxAPin = GPIO_NUM_27;// LSB of the Mux
+const gpio_num_t MuxBPin = GPIO_NUM_14;
+const gpio_num_t MuxCPin = GPIO_NUM_12;// MSB of the Mux
+const gpio_num_t MuxSigPin = GPIO_NUM_13;
+
+const gpio_num_t motorPins[] = {GPIO_NUM_19, GPIO_NUM_18, GPIO_NUM_5, GPIO_NUM_17, GPIO_NUM_16, GPIO_NUM_4, GPIO_NUM_2, GPIO_NUM_15};
+
+
+
+//======================================================================================================
+//	Enums
+//======================================================================================================
+
+
+
+
+
+
+
+//======================================================================================================
+//	Structs
+//======================================================================================================
+
+
+
+
+
+//======================================================================================================
+//	Variables
+//======================================================================================================
+
+#define WIFI_SSID "Dist-Hat" // Set the SSID of the WiFi network
+#define WIFI_PASSWORD NULL // Set the password of the WiFi network
+
+// Web Server Variables
+AsyncWebServer server(80);// Create AsyncWebServer object on port 80
+AsyncWebSocket ws("/ws");// Create a WebSocket object on path "ip:80/ws"
+
+
+// Web UI Data
+JsonDocument messageJSON;			// JSON Object to store data to send to the web UI
+SemaphoreHandle_t jsonSemaphore;	// Semaphore to control access to the JSON object
+
+// Task Variables
+TaskHandle_t sendDataToUITask;
+
+// Event Group for Tasks and related Defines
+typedef enum{
+	SEND_DATA = 0x01
+} EventFlags;
+
+EventGroupHandle_t mainEventGroup;
+
+
+// Distance Sensor Variables
+long duration[NUM_SENSORS], cm[NUM_SENSORS], inches[NUM_SENSORS];
+
+const uint16_t MaxDurration = 30000;// Maximum duration for the pulseIn function to wait for a pulse. This is in microseconds
+
+
+// Motor Variables
+const uint16_t motorPWMFreq = 5000;				// Frequency for the PWM Output signal to the motors. This will also work with up to a max resolution of 13 bits
+const uint8_t motorPWMResolution = 10;			// 10 bit resolution for PWM. We may want to try a higher resolution at some point
+const uint16_t motorAbsMaxSpeed = (1 << (motorPWMResolution)) - 1;// Absolute maximum speed of the motor. 1023 for 10 bit PWM, 4095 for 12 bit PWM. This is the maximum value that can be sent to the motor driver
+
+
+const TickType_t readSensorsInterval = pdMS_TO_TICKS(1000);// Time in ms to wait between reading the sensors
+TickType_t lastLoopStartTime = 0;// Time in ms that the last loop started
+
+
+//======================================================================================================
+//	Setup Functions
+//======================================================================================================
+
+// Function to setup the WiFi connection
+void initWiFi(){
+	WiFi.mode(WIFI_AP);// Set WiFi mode to Station (Connecting to some other access point, ie a laptop's hotspot)
+	// WiFi.config(IPAddress(192,168,137,2), IPAddress(192,168,137,1),IPAddress(255,255,255,0));// Set Static IP Address (IP, Gateway, Subnet Mask). NOTE: This line does not work on Laptop Hotspot
+	// WiFi.begin(WIFI_SSID, WIFI_PASSWORD);// Start Wifi Connection
+	WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);// Start Wifi Connection
+	// while(WiFi.status() != WL_CONNECTED){// Wait for WiFi to connect
+	// 	delay(500);
+	// 	#if SERIAL_ENABLED
+	// 		Serial.print(".");
+	// 	#endif
+	// }
+	#if SERIAL_ENABLED
+		Serial.println("\nConnected to WiFi\n");
+	#endif
+}
+
+
+
+
+
+//======================================================================================================
+//	Web Socket Server Functions
+//======================================================================================================
+
+// Function to send data to all clients on the web UI
+void notifyClients(String data) {
+	#if SERIAL_ENABLED
+		Serial.println("Sending Data to Web UI: " + data + "\n");
+	#endif
+	ws.textAll(data);
+}
+
+
+
+
+
+// Function to handle the verified content of the WebSocket Messages. This is run in the Web Server thread with priority 3
+void selectCommand(char* msg){
+	JsonDocument doc; // Create a JSON document to store the message
+	DeserializationError error = deserializeJson(doc, msg);// Deserialize the JSON message
+
+	if(error){// If there was an error deserializing the JSON message
+		#if SERIAL_ENABLED
+			Serial.print(F("deserializeJson() failed: "));
+			Serial.println(error.c_str());
+		#endif
+		ws.textAll("{\"message\": \"Invalid JSON received\"}");// Send a message to the web UI that the JSON was invalid
+		return;
+	}
+
+	const char* cmd = doc["cmd"];// Get the command from the JSON message
+
+	// Check what the message is and set the appropriate bit in the event group
+	{
+		// If the command is not recognized, send a message to the web UI
+		ws.textAll("{\"message\": \"Invalid Command Received\"}");
+		// xEventGroupSetBits(mainEventGroup, SEND_DATA);// Set the SEND_DATA bit to send data to the web UI
+	}
+}
+
+
+
+// AwsEventHandler Function. This largely comes from the ESPAsyncWebServer Documentation example.
+void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+	if(type == WS_EVT_CONNECT){
+		//client connected
+		#if SERIAL_ENABLED
+			Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+		#endif
+		client->printf("{\"message\": \"Hello Client %u :)\"}", client->id());
+		client->ping();
+	} else if(type == WS_EVT_DISCONNECT){
+		//client disconnected
+		#if SERIAL_ENABLED
+			Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+		#endif
+	} else if(type == WS_EVT_ERROR){
+		//error was received from the other end
+		#if SERIAL_ENABLED
+			Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+		#endif
+	} else if(type == WS_EVT_PONG){
+		//pong message was received (in response to a ping request maybe)
+		#if SERIAL_ENABLED
+			Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+		#endif
+	} else if(type == WS_EVT_DATA){
+		//data packet
+		AwsFrameInfo * info = (AwsFrameInfo*)arg;
+		if(info->final && info->index == 0 && info->len == len){
+			//the whole message is in a single frame and we got all of it's data
+			#if SERIAL_ENABLED
+				Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+			#endif
+			if(info->opcode == WS_TEXT){
+				data[len] = 0;// Set last character to null terminator
+				#if SERIAL_ENABLED
+					Serial.printf("%s\n", (char*)data);
+				#endif
+				selectCommand((char*)data);
+			} else {
+				for(size_t i=0; i < info->len; i++){
+					#if SERIAL_ENABLED
+						Serial.printf("%02x ", data[i]);
+					#endif
+				}
+				#if SERIAL_ENABLED
+					Serial.printf("\n");
+				#endif
+			}
+			// if(info->opcode == WS_TEXT)
+			// 	client->text("{\"message\": \"I got your text message\"}");
+			// else
+			// 	client->binary("{\"message\": \"I got your binary message\"}");
+		} else {
+			//message is comprised of multiple frames or the frame is split into multiple packets
+			if(info->index == 0){
+				if(info->num == 0){
+					#if SERIAL_ENABLED
+						Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+					#endif
+				}
+				#if SERIAL_ENABLED
+					Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+				#endif
+			}
+
+			#if SERIAL_ENABLED
+				Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+			#endif
+			if(info->message_opcode == WS_TEXT){
+				data[len] = 0;
+				#if SERIAL_ENABLED
+					Serial.printf("%s\n", (char*)data);
+				#endif
+			} else {
+				for(size_t i=0; i < len; i++){
+					#if SERIAL_ENABLED
+						Serial.printf("%02x ", data[i]);
+					#endif
+				}
+				#if SERIAL_ENABLED
+					Serial.printf("\n");
+				#endif
+			}
+
+			if((info->index + len) == info->len){
+				#if SERIAL_ENABLED
+					Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+				#endif
+				if(info->final){
+					#if SERIAL_ENABLED
+						Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+					#endif
+					// if(info->message_opcode == WS_TEXT)
+					// 	client->text("{\"message\": \"I got your text message\"}");
+					// else
+					// 	client->binary("{\"message\": \"I got your binary message\"}");
+				}
+			}
+		}
+	}
+}
+
+
+
+void initWebSocket() {
+	ws.onEvent(onEvent);
+	server.addHandler(&ws);
+}
+
+
+
+
+// Task to Send Data to Web UI
+void sendDataToUI(void *pvParameters){
+	#if SERIAL_ENABLED
+		Serial.println("Send Data to UI Task Running");
+	#endif
+	while(true){
+		// Wait for the SEND_DATA bit to be set
+		EventBits_t bits = xEventGroupWaitBits(mainEventGroup, SEND_DATA, pdTRUE, pdFALSE, portMAX_DELAY);
+		if((bits & SEND_DATA) == SEND_DATA){
+			#if SERIAL_ENABLED
+				Serial.println("sendDataToUI Task: Sending Data to Web UI Triggered\n");
+			#endif
+
+			xSemaphoreTake(jsonSemaphore, portMAX_DELAY);// Wait for the JSON object to be free (not being sent to the web UI
+
+			String tmp;
+			serializeJson(messageJSON, tmp);
+			messageJSON.clear();// Clears the data that was stored in messageJSON, so it will be empty for the next time we want to send data
+			xSemaphoreGive(jsonSemaphore);// Give the JSON object back to the main control loop
+
+			notifyClients(tmp);// Send the JSON data to the web UI
+		}
+	}
+}
+
+
+
+
+
+//======================================================================================================
+//	Helper Functions
+//======================================================================================================
+
+void selectMuxChannel(uint8_t channel){
+	digitalWrite(MuxAPin, channel & 0x01);
+	digitalWrite(MuxBPin, (channel >> 1) & 0x01);
+	digitalWrite(MuxCPin, (channel >> 2) & 0x01);
+}
+
+
+
+void triggerSensor(){
+	digitalWrite(MuxSigPin, LOW);
+	delayMicroseconds(5);
+	digitalWrite(MuxSigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(MuxSigPin, LOW);
+}
+
+
+
+
+void readDistance(uint8_t sensorNum){
+	// Read the distance from the sensor
+	duration[sensorNum] = pulseIn(echoPins[sensorNum], HIGH);
+	cm[sensorNum] = (duration[sensorNum]/2) / 29.1;
+	inches[sensorNum] = (duration[sensorNum]/2) / 74;
+
+	#if SERIAL_ENABLED
+		Serial.printf("Distance for Sensor %d: ", sensorNum);
+		Serial.print(cm[sensorNum]);
+		Serial.print("cm");
+		Serial.print(inches[sensorNum]);
+		Serial.print("in");
+		Serial.println();
+	#endif
+
+}
+
+
+
+
+void setMotorPWM(uint8_t motor){
+	uint16_t tmp = map(duration[motor], 0, MaxDurration, 0, motorAbsMaxSpeed);// Map the duration to the motor speed
+	ledcWrite(motor, tmp);	// Set the PWM signal to the motor
+}
+
+
+
+
+
+//======================================================================================================
+//	 Main Code
+//======================================================================================================
+
+void setup(){
+	// if SERIAL_ENABLED is defined, then Serial will be enabled	
+	#if SERIAL_ENABLED
+		Serial.begin(115200);
+	#endif
+
+	initWiFi();
+	jsonSemaphore = xSemaphoreCreateMutex();// Create a semaphore to control
+	if( jsonSemaphore != NULL ) {
+		// The semaphore was created successfully.
+		// The semaphore can now be used.
+		#if SERIAL_ENABLED
+			Serial.println("Semaphore Created Successfully");
+		#endif
+	}else{
+		// The semaphore was not created because there was not enough FreeRTOS heap available.
+		// Take appropriate action.
+		#if SERIAL_ENABLED
+			Serial.println("Semaphore Creation Failed");
+		#endif
+	}
+
+	ElegantOTA.begin(&server);	// Prep the ElegantOTA server (Over The Air updates)
+	initWebSocket();			// Prep the WebSocket server
+	server.begin();				// Start the web server. Automatically makes a new task at priority 3, on whatever core is available.
+
+	// Create the event group
+	mainEventGroup = xEventGroupCreate();
+
+	// Create the task to send data to the web UI
+	xTaskCreatePinnedToCore(
+		sendDataToUI,		// Task Function
+		"Send Data to Server",	// Task Name
+		10000,					// Stack Size, should check utilization later with uxTaskGetStackHighWaterMark
+		NULL,					// Parameters
+		3,						// Priority 3, so it is at the same priority as the recieving data task. This is to help prevent the two from getting locked up
+		&sendDataToUITask,		// Task Handle
+		0						// Core 0
+	);
+
+
+
+	// Init pins
+	for(uint8_t i = 0; i < NUM_SENSORS; i++){
+		// Set Up Echo Pins
+		pinMode(echoPins[i], INPUT);
+
+		// Set up Motor Pins
+		pinMode(motorPins[i], OUTPUT);
+		ledcSetup(i, motorPWMFreq, motorPWMResolution);	// Setup PWM for the motor
+		ledcAttachPin(motorPins[i], i);					// Attach the PWM signal to the pin
+		ledcWrite(i, 0);								// Set the PWM signal to 0
+	}
+
+	pinMode(MuxAPin, OUTPUT);
+	pinMode(MuxBPin, OUTPUT);
+	pinMode(MuxCPin, OUTPUT);
+	pinMode(MuxSigPin, OUTPUT);
+
+	// Set the Last Loop Start Time
+	lastLoopStartTime = xTaskGetTickCount();// Get the current time in tick count
+}
+
+
+
+
+void loop(){
+	for(int i = 0; i < NUM_SENSORS; i++){
+		selectMuxChannel(i);
+		triggerSensor();
+		readDistance(echoPins[i]);
+		setMotorPWM(i);
+	}
+
+	// prep data to send to web server
+	// JsonArray durVals = messageJSON["durVals"].to<JsonArray>();
+	// JsonArray distCM = messageJSON["distCM"].to<JsonArray>();
+
+	BaseType_t status = xSemaphoreTake(jsonSemaphore, readSensorsInterval/2);// Wait for the JSON object to be free (not being sent to the web UI), for half the time between reads
+	
+	if(status == pdTRUE){											// If the semaphore was aquired, send stuff to the web UI
+		JsonArray distIN = messageJSON["distIN"].to<JsonArray>();
+		for(int i = 0; i < NUM_SENSORS; i++){
+			// durVals.add(duration[i]);
+			// distCM.add(cm[i]);
+			distIN.add(inches[i]);
+		}
+		xSemaphoreGive(jsonSemaphore);// Give the JSON object back to the main control loop
+		xEventGroupSetBits(mainEventGroup, SEND_DATA);// Set the SEND_DATA bit to send data to the web UI
+	}
+
+	xTaskDelayUntil(&lastLoopStartTime, readSensorsInterval);// Delay for readSensorsInterval between reads
+}
---
+++ data/main.js
@@ -0,0 +1,213 @@
+/*
+	This file contains the main JavaScript code for the web interface of the robot.
+	Much of the basis for this code came from Rui Santos's ESP32 tutorials: https://randomnerdtutorials.com/projects-esp32/
+	Modifications made by Collin Schofield, with assistance from Github Copilot.
+*/
+
+//=======================================================
+// Global variables
+//=======================================================
+
+var botPath = `ws://${window.location.hostname}/ws`;
+var websocket;
+
+window.addEventListener('load', onload);// Initialize the websocket when the page is loaded
+
+const outChartEnum = Object.freeze({// Enum for the series's of the output chart
+	pOut: 0,
+	p2Out: 1,
+	dOut: 2,
+	iOut: 3,
+	out: 4
+});
+
+
+var distValsIN = [];// Array to store the sensor values for the chart
+var distValsINWithSpacers = [];// Array to store the sensor values for the chart, with spacers between sensors
+// Charts for the sensor values, error values, and output values
+var lineDistanceChart;
+var distanceChart;
+
+
+const sensorWidth = 15;// Width of the sensor in degrees
+const numSensors = 8;// Number of sensors
+const numFrontSensors = 7;// Number of front sensors
+const numRearSensors = numSensors - numFrontSensors;// Number of rear sensors
+const frontSpace = (180 - (numFrontSensors * sensorWidth))/(numFrontSensors - 1);// Space for the front sensors
+const rearSpace = (180 - (numRearSensors * sensorWidth))/(numRearSensors + 1);// Space for the rear sensors
+
+
+
+//=======================================================
+// WebSocket Functions
+//=======================================================
+
+function onload(event) {
+	initWebSocket();
+	setTimeout(prepCharts, 1000);
+}
+
+
+
+// Function to initialize the WebSocket connection
+function initWebSocket() {
+	console.log('Trying to open a WebSocket connection…');
+	websocket = new WebSocket(botPath);
+	websocket.onopen = onOpen;
+	websocket.onclose = onClose;
+	websocket.onmessage = onMessage;
+}
+
+
+
+// Actions to take when the WebSocket connection is opened
+function onOpen(event) {
+	console.log('Websocket Connection opened');
+	document.getElementsByClassName('topnav')[0].style.backgroundColor = 'green';
+	setTimeout(getState, 1000);
+}
+
+
+
+// Actions to take when the WebSocket connection is closed
+function onClose(event) {
+	console.log('Websocket Connection closed');
+	setTimeout(initWebSocket, 2000);
+	document.getElementsByClassName('topnav')[0].style.backgroundColor = 'red';
+}
+
+
+
+// Function that receives the message from the ESP32 with the readings
+function onMessage(event) {
+	try {
+		tmp = JSON.parse(event.data);
+		if(tmp.message == undefined){
+			console.log(tmp);// Print the whole JSON of recieved data in the console, if it is not just a message
+		}
+	}catch(e){// If the JSON parsing fails, print the error and the data in the console
+		console.log("Error parsing JSON: " + e);
+		console.log(event.data);
+	}
+	var receivedObj = JSON.parse(event.data);
+	var keys = Object.keys(receivedObj);
+
+	for (var i = 0; i < keys.length; i++){// Loop through all keys in the received object
+		var key = keys[i];
+		if(key == "message"){// If the key is message, print the message in the console
+			console.log(receivedObj[key]);
+			continue;
+		}else{// Otherwise, update the value of each element in the HTML with the id of the key
+			// Check for special keys
+			if(key == "distIN"){// If the key is distIN, update the values of the sensor values in the JavaScript array
+				distValsIN = receivedObj[key];
+				distanceChart.series[0].setData(distValsIN);
+				continue;
+			}
+
+			try{// Try to update the element with the id of the key
+				document.getElementById(key).innerHTML = receivedObj[key];
+			}catch(e){
+				console.log("Error updating element with id " + key + ": " + e);
+			}
+		}
+	}
+}
+
+
+
+
+
+//=======================================================
+// Other Functions
+//=======================================================
+
+function prepCharts(){
+	// distanceChart = new Highcharts.Chart({
+	// 	chart: {
+	// 		type: 'variablepie'
+	// 	},
+	// 	title: {
+	// 		text: 'ROund Distance Chart',
+	// 		align: 'left'
+	// 	},
+	// 	// tooltip: {
+	// 	// 	headerFormat: '',
+	// 	// 	pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+	// 	// 		'Area (square km): <b>{point.y}</b><br/>' +
+	// 	// 		'Population density (people per square km): <b>{point.z}</b><br/>'
+	// 	// },
+	// 	plotOptions: {
+	// 		variablepie: {
+	// 			startAngle: -90,
+	// 			dataLabels: {
+	// 				enabled: false,
+	// 			}
+	// 		}
+	// 	},
+	// 	series: [{
+	// 		minPointSize: 1,
+	// 		innerSize: '20%',
+	// 		zMin: 0,
+	// 		name: 'Distance (in) (round graph)',
+	// 		borderRadius: 5,
+	// 		yAxis:{
+	// 			title: {
+	// 				text: "Angle (degrees)"
+	// 			},
+	// 			labels: {
+	// 				format: '{value} deg'
+	// 			},
+
+				
+	// 		},
+	// 		series:[{
+	// 			name: 'Distance (in)',
+	// 			data: distValsINWithSpacers
+	// 		}]
+	// 	}]
+	// });
+
+
+
+
+	lineDistanceChart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'chart-distance-line',
+			type: 'spline',
+			animation: false
+		},
+		title: { text: 'Distance (in)' },
+		xAxis:{
+			categories: [0,1,2,3,4,5,6,7]
+		},
+		yAxis: {
+			title: {
+				text: 'Distance'
+			},
+			labels: {
+				format: '{value} in'
+			},
+			min: 0,
+			max: 156
+		},
+		plotOptions: {
+			spline: {
+				marker: {
+					radius: 4,
+					lineColor: '#666666',
+					lineWidth: 1
+				}
+			},
+		},
+		series: [{
+			name: 'Distance Readings',
+			marker: {
+				symbol: 'diamond'
+			},
+			data: distValsIN
+		}]
+	});
+
+
+}
